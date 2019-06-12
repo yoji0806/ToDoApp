22d6c6ad3f098b2f5d3ae38024c02bef7d3a13a8
yoji0806 <baske0806@outlook.jp>
2019-06-12 22:40:04 +0900

    make UI

--- app/src/main/java/com/yoji0806/to_doapp/MainActivity.kt
+++ app/src/main/java/com/yoji0806/to_doapp/MainActivity.kt
@@ -1,39 +1,63 @@
 package com.yoji0806.to_doapp
 
+import android.content.Context
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
-
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+
+
+        edit_taskTitle.visibility = View.INVISIBLE
+
+
+        var lowPosition = true
+
+        fab.setOnClickListener {
+
+            if (lowPosition) {
+                edit_taskTitle.visibility = View.VISIBLE
+                showSoftKeyboard(edit_taskTitle)
+            }else{
+                edit_taskTitle.visibility = View.INVISIBLE
+                hideSoftKeyboard(edit_taskTitle)
+            }
+
+            lowPosition = !lowPosition
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+
+    private fun hideSoftKeyboard(view:View) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, InputMethodManager.HIDE_IMPLICIT_ONLY)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+
+    private fun showSoftKeyboard(view:View) {
+        if (view.requestFocus())
+        {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
         }
     }
+
+
+
 }
+
+
+
+
+
+class MainAdapter
